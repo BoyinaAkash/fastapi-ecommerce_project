69ac1fe6d9fab19049a4be0eb163b35cad279ea6
BoyinaAkash <akashboyina@gmail.com>
2026-09-05 18:45:19 +0530

    Fix typo in DATABASE_URL connection string

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker , declarative_base
 # DATABASE_URL="mysql+pymysql://root:root@localhost:3306/ecommerce_db"
-DATABASE_URL="mysql+pymsql://avnadmin:AVNS_xObL3o43wV0W1AXDUms@akash12ma-akashboyina-2925.a.aivencloud.com:17823/defaultdb
+DATABASE_URL="mysql+pymysql://avnadmin:AVNS_xObL3o43wV0W1AXDUms@akash12ma-akashboyina-2925.a.aivencloud.com:17823/defaultdb
 engine=create_engine(DATABASE_URL)
 SessionLocal=sessionmaker(
     autocommit = False,
