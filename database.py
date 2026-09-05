bc0ec867a0ade4e8f98fdeebe7c25bba80ee715a
BoyinaAkash <akashboyina@gmail.com>
2026-09-05 18:24:45 +0530

    modified database.py to update the DATABASE_URL for MySQL connection with Aiven cloud database credentials.

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker , declarative_base
 # DATABASE_URL="mysql+pymysql://root:root@localhost:3306/ecommerce_db"
-DATABASE_URL=mysql://avnadmin:AVNS_xObL3o43wV0W1AXDUms@akash12ma-akashboyina-2925.a.aivencloud.com:17823/defaultdb?ssl-mode=REQUIRED
+DATABASE_URL="mysql://avnadmin:AVNS_xObL3o43wV0W1AXDUms@akash12ma-akashboyina-2925.a.aivencloud.com:17823/defaultdb?ssl-mode=REQUIRED"
 engine=create_engine(DATABASE_URL)
 SessionLocal=sessionmaker(
     autocommit = False,
